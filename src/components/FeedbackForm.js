a906febe89f7559c2a49924ec2505ac7e0605905
Yurii <yurii@MacBook-Pro-2.local>
2023-04-20 12:42:59 +0300

    initial react setup, testing mui

---
+++ src/components/FeedbackForm.js
@@ -0,0 +1,11 @@
+import { Paper } from "@mui/material";
+
+export default function FeedbackForm() {
+  return (
+    <div>
+      <Paper elevation={16}>
+        <h3>fasfasf</h3>
+      </Paper>
+    </div>
+  );
+}
