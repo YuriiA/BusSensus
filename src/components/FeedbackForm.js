62285163e5d6d56cc655b14988cd3c7b59a3a5b1
Yurii <yurii@MacBook-Pro-2.local>
2023-04-27 13:52:41 +0300

    add form validation/display errors if inputs are not selected,prevent submit of the incomplete form. Fake post to jsonplaceholder

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -13,31 +13,52 @@ import shortid from "shortid";
 
 export default function FeedbackForm() {
   const [busNumber, setBusNumber] = useState([""]);
-  const [busNoToSend, setBusNoToSend] = useState([]);
-
   const [busRoute, setBusRoute] = useState([""]);
-  const [busRouteToSend, setBusRouteToSend] = useState([]);
-
   const [station, setStation] = useState([""]);
-  const [stationToSend, setStationToSend] = useState([]);
-
   const [numOfPassengers, setNumOfPassengers] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const time = dateBuilder(new Date());
-    setNumOfPassengers("");
-    const userId = shortid.generate();
+  const [feedback, setFeedback] = useState({
+    busNoToSend: "",
+    busRouteToSend: "",
+    stationToSend: "",
+  });
 
-    console.log(
-      busNoToSend,
-      busRouteToSend,
-      stationToSend,
-      numOfPassengers,
-      time,
-      `user id: ${userId}`
-    );
+  const [errors, setErrors] = useState({
+    busNo: "",
+    route: "",
+    busStation: "",
+    passengers: "",
+  });
+
+  function isFormValid() {
+    let isValid = true;
+    let newErrors = { ...errors };
+
+    if (feedback.busNoToSend.length === 0) {
+      isValid = false;
+      newErrors.busNo = "Bus Number is required";
+    }
+
+    if (feedback.busRouteToSend.length === 0) {
+      isValid = false;
+      newErrors.route = "Please select bus route ";
+    }
+
+    if (feedback.stationToSend.length === 0) {
+      isValid = false;
+      newErrors.busStation = "Please select the station";
+    }
+
+    if (numOfPassengers === "") {
+      isValid = false;
+      newErrors.passengers = "Please enter the number of passengers";
+    }
+
+    setErrors(newErrors);
+
+    return isValid;
   }
+
   useEffect(() => {
     fetch(" https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -61,6 +82,38 @@ export default function FeedbackForm() {
       });
   }, []);
 
+  function handleChange(e) {
+    setFeedback({ ...feedback, [e.target.name]: e.target.value });
+    setErrors(" ");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const time = dateBuilder(new Date());
+    const userId = shortid.generate();
+
+    if (!isFormValid()) {
+      return;
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        busNo: feedback.busNoToSend,
+        route: feedback.busRouteToSend,
+        station: feedback.stationToSend,
+        numOfPass: numOfPassengers,
+        date: time,
+        user: userId,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+    setNumOfPassengers("");
+  }
   return (
     <div>
       <Paper elevation={16}>
@@ -68,10 +121,10 @@ export default function FeedbackForm() {
           <FormControl>
             <h4> Bus Number</h4>
             <Select
-              id="busNumber"
-              value={busNoToSend || " "}
-              name="busNumber"
-              onChange={(e) => setBusNoToSend(e.target.value)}
+              id="busNoToSend"
+              value={feedback.busNoToSend || " "}
+              name="busNoToSend"
+              onChange={handleChange}
             >
               {busNumber.map((number) => {
                 return (
@@ -81,15 +134,18 @@ export default function FeedbackForm() {
                 );
               })}
             </Select>
+            {errors.busNo && (
+              <div className="invalid-feedback">{errors.busNo}</div>
+            )}
           </FormControl>
 
           <FormControl>
             <h4> Bus Route</h4>
             <Select
-              id="busRoute"
-              value={busRouteToSend || " "}
-              name="busRoute"
-              onChange={(e) => setBusRouteToSend(e.target.value)}
+              id="busRouteToSend"
+              value={feedback.busRouteToSend || " "}
+              name="busRouteToSend"
+              onChange={handleChange}
             >
               {busRoute.map((route) => {
                 return (
@@ -99,15 +155,18 @@ export default function FeedbackForm() {
                 );
               })}
             </Select>
+            {errors.route && (
+              <div className="invalid-feedback">{errors.route}</div>
+            )}
           </FormControl>
 
           <FormControl>
             <h4> Station</h4>
             <Select
-              id="station"
-              value={stationToSend || " "}
-              name="station"
-              onChange={(e) => setStationToSend(e.target.value)}
+              id="stationToSend"
+              value={feedback.stationToSend || " "}
+              name="stationToSend"
+              onChange={handleChange}
             >
               {station.map((BusStation) => {
                 return (
@@ -117,6 +176,9 @@ export default function FeedbackForm() {
                 );
               })}
             </Select>
+            {errors.busStation && (
+              <div className="invalid-feedback">{errors.busStation}</div>
+            )}
           </FormControl>
           <FormControl>
             <h4>No of people in the bus</h4>
@@ -127,6 +189,9 @@ export default function FeedbackForm() {
               value={numOfPassengers}
               onChange={(e) => setNumOfPassengers(e.target.value)}
             />
+            {errors.passengers && (
+              <div className="invalid-feedback">{errors.passengers}</div>
+            )}
           </FormControl>
           <Button onClick={handleSubmit}>Submit</Button>
         </FormGroup>
