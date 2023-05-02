03019cdd26d10f28fc9577aece2784f374999f7a
Yurii <yurii@MacBook-Pro-2.local>
2023-05-02 15:12:32 +0300

    install json-server with different routes for each input, hide input while previous one is not selected/changed and only after fetch the values for menuItem, add different states for each input, selected value to send, fetch error, loading, remove userId, form validation errors dispalyed using FormHelperText, remove id from select, initial state changed to an empty array

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <FeedbackForm />
-      </Container>
-    </div>
+    <Container>
+      <FeedbackForm />
+    </Container>
   );
 }
 
--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,27 +1,32 @@
 import { useEffect, useState } from "react";
 import {
-  Paper,
   FormGroup,
   FormControl,
   Select,
   TextField,
   MenuItem,
   Button,
+  FormHelperText,
 } from "@mui/material";
-import { dateBuilder } from "../utils/utils";
+// import { dateBuilder } from "../utils/utils";
 import shortid from "shortid";
 
 export default function FeedbackForm() {
-  const [busNumber, setBusNumber] = useState([""]);
-  const [busRoute, setBusRoute] = useState([""]);
-  const [station, setStation] = useState([""]);
-  const [numOfPassengers, setNumOfPassengers] = useState("");
+  const [loading, setLoading] = useState("true");
 
-  const [feedback, setFeedback] = useState({
-    busNoToSend: "",
-    busRouteToSend: "",
-    stationToSend: "",
-  });
+  const [busNumber, setBusNumber] = useState([]);
+  const [busNoToSend, setBusNoToSend] = useState([]);
+  const [busNumberError, setBusNumberError] = useState([]);
+
+  const [busRoute, setBusRoute] = useState([]);
+  const [busRouteToSend, setBusRouteToSend] = useState([]);
+  const [busRouteError, setBusRouteError] = useState([]);
+
+  const [station, setStation] = useState([]);
+  const [stationToSend, setStationToSend] = useState([]);
+  const [stationError, setStationError] = useState([]);
+
+  const [numOfPassengers, setNumOfPassengers] = useState("");
 
   const [errors, setErrors] = useState({
     busNo: "",
@@ -34,17 +39,17 @@ export default function FeedbackForm() {
     let isValid = true;
     let newErrors = { ...errors };
 
-    if (feedback.busNoToSend.length === 0) {
+    if (busNoToSend.length === 0) {
       isValid = false;
       newErrors.busNo = "Bus Number is required";
     }
 
-    if (feedback.busRouteToSend.length === 0) {
+    if (busRouteToSend.length === 0) {
       isValid = false;
       newErrors.route = "Please select bus route ";
     }
 
-    if (feedback.stationToSend.length === 0) {
+    if (stationToSend.length === 0) {
       isValid = false;
       newErrors.busStation = "Please select the station";
     }
@@ -59,93 +64,120 @@ export default function FeedbackForm() {
     return isValid;
   }
 
+  //Fetch Bus numbers:
+
   useEffect(() => {
-    fetch(" https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setBusNumber(
-          data.map((item) => {
-            return item.id;
-          })
-        );
-        setBusRoute(
-          data.map((item) => {
-            return item.username;
-          })
-        );
+    try {
+      fetch(" http://localhost:3001/busNo")
+        .then((res) => res.json())
+        .then((data) => setBusNumber(data), setLoading(false));
+    } catch (err) {
+      setBusNumberError(err);
+      console.log(busNumberError);
+    }
+  }, [busNumberError]);
 
-        setStation(
-          data.map((item) => {
-            return item.address.street;
-          })
-        );
-      });
-  }, []);
+  //Set bus number to send and Fetch Bus Route
 
-  function handleChange(e) {
-    setFeedback({ ...feedback, [e.target.name]: e.target.value });
-    setErrors(" ");
+  function handleBusNoChange(e) {
+    setBusNoToSend(e.target.value);
+    setErrors("");
+
+    try {
+      fetch(" http://localhost:3001/routes")
+        .then((res) => res.json())
+        .then((data) => setBusRoute(data));
+    } catch (err) {
+      setBusRouteError(err);
+      console.log(busRouteError);
+    }
   }
 
+  // Set bus route to send and Fetch Stations
+
+  function handleRouteChange(e) {
+    setBusRouteToSend(e.target.value);
+    setErrors("");
+
+    try {
+      fetch(" http://localhost:3001/stations")
+        .then((res) => res.json())
+        .then((data) => setStation(data));
+    } catch (err) {
+      setStationError(err);
+      console.log(stationError);
+    }
+  }
+
+  function handleStationsChange(e) {
+    setStationToSend(e.target.value);
+    setErrors("");
+  }
+
+  function handlePassengersChange(e) {
+    setNumOfPassengers(e.target.value);
+    setErrors("");
+  }
   function handleSubmit(e) {
     e.preventDefault();
-    const time = dateBuilder(new Date());
-    const userId = shortid.generate();
-
+    // const time = dateBuilder(new Date());
+    console.log(errors);
     if (!isFormValid()) {
       return;
     }
 
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: JSON.stringify({
-        busNo: feedback.busNoToSend,
-        route: feedback.busRouteToSend,
-        station: feedback.stationToSend,
-        numOfPass: numOfPassengers,
-        date: time,
-        user: userId,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
+    console.log(numOfPassengers);
+
+    //   fetch("https://jsonplaceholder.typicode.com/posts", {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       busNo: feedback.busNoToSend,
+    //       route: feedback.busRouteToSend,
+    //       station: feedback.stationToSend,
+    //       numOfPass: numOfPassengers,
+    //       date: time,
+    //     }),
+    //     headers: {
+    //       "Content-type": "application/json; charset=UTF-8",
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
     setNumOfPassengers("");
   }
+
+  if (loading) {
+    return "form loading";
+  }
+
   return (
     <div>
-      <Paper elevation={16}>
-        <FormGroup>
-          <FormControl>
-            <h4> Bus Number</h4>
-            <Select
-              id="busNoToSend"
-              value={feedback.busNoToSend || " "}
-              name="busNoToSend"
-              onChange={handleChange}
-            >
-              {busNumber.map((number) => {
-                return (
-                  <MenuItem key={shortid.generate()} value={number}>
-                    {number}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-            {errors.busNo && (
-              <div className="invalid-feedback">{errors.busNo}</div>
-            )}
-          </FormControl>
+      <FormGroup>
+        <FormControl error={errors.busNo ? true : false}>
+          <h4> Bus Number</h4>
+          <Select
+            value={busNoToSend || " "}
+            name="busNoToSend"
+            onChange={handleBusNoChange}
+          >
+            {busNumber.map((number) => {
+              return (
+                <MenuItem key={shortid.generate()} value={number}>
+                  {number}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          {errors.busNo && <FormHelperText> {errors.busNo}</FormHelperText>}
+        </FormControl>
 
-          <FormControl>
+        {busNoToSend.length !== 0 && (
+          <FormControl error={errors.route ? true : false}>
             <h4> Bus Route</h4>
             <Select
-              id="busRouteToSend"
-              value={feedback.busRouteToSend || " "}
+              value={busRouteToSend || " "}
               name="busRouteToSend"
-              onChange={handleChange}
+              onChange={handleRouteChange}
             >
               {busRoute.map((route) => {
                 return (
@@ -155,18 +187,17 @@ export default function FeedbackForm() {
                 );
               })}
             </Select>
-            {errors.route && (
-              <div className="invalid-feedback">{errors.route}</div>
-            )}
+            {errors.route && <FormHelperText> {errors.route}</FormHelperText>}
           </FormControl>
+        )}
 
-          <FormControl>
+        {busRouteToSend.length !== 0 && (
+          <FormControl error={errors.busStation ? true : false}>
             <h4> Station</h4>
             <Select
-              id="stationToSend"
-              value={feedback.stationToSend || " "}
+              value={stationToSend || " "}
               name="stationToSend"
-              onChange={handleChange}
+              onChange={handleStationsChange}
             >
               {station.map((BusStation) => {
                 return (
@@ -177,25 +208,28 @@ export default function FeedbackForm() {
               })}
             </Select>
             {errors.busStation && (
-              <div className="invalid-feedback">{errors.busStation}</div>
+              <FormHelperText>{errors.busStation}</FormHelperText>
             )}
           </FormControl>
-          <FormControl>
+        )}
+        {stationToSend.length !== 0 && (
+          <FormControl error={errors.passengers ? true : false}>
             <h4>No of people in the bus</h4>
             <TextField
-              id="numOfPassengers"
               type="number"
               name="numOfPassengers"
-              value={numOfPassengers}
-              onChange={(e) => setNumOfPassengers(e.target.value)}
+              value={numOfPassengers || ""}
+              onChange={handlePassengersChange}
+              error={errors.passengers ? true : false}
             />
             {errors.passengers && (
-              <div className="invalid-feedback">{errors.passengers}</div>
+              <FormHelperText>{errors.passengers}</FormHelperText>
             )}
           </FormControl>
-          <Button onClick={handleSubmit}>Submit</Button>
-        </FormGroup>
-      </Paper>
+        )}
+
+        <Button onClick={handleSubmit}>Submit</Button>
+      </FormGroup>
     </div>
   );
 }
